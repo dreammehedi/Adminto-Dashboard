602f7a12e89c3b7fcd03232794c8fbe8802ee595
dreammehedi <dreammehedihassan@gmail.com>
2024-08-24 22:41:50 +0600

    Update Sign In Page Ui

--- src/pages/signin/SignIn.jsx
+++ src/pages/signin/SignIn.jsx
@@ -1,9 +1,9 @@
-function SignIn() {
+export default function SignIn() {
   return (
-    <>
-      <div className="bg-[#313844]">
-        <h1 className="text-white font-semibold  text-center my-12">SIGN IN</h1>
-        <form className="flex flex-col px-6">
+    <div className="flex items-center h-screen">
+      <div className="bg-[#313844]  lg:w-1/3 mx-auto rounded-md ">
+        <h1 className="text-white font-semibold  text-center py-12">SIGN IN</h1>
+        <form className="flex flex-col px-12 ">
           <label
             className="text-[#95989b] text-sm font-semibold mb-3"
             htmlFor="email"
@@ -12,29 +12,38 @@ function SignIn() {
           </label>
           <input
             placeholder="email address.."
-            className="outline-none rounded text-[#95989b] border-gray-600 border p-2 text-sm bg-[#363e4b]"
+            className="outline-none rounded text-[#95989b] border-gray-600 border p-2 text-sm bg-[#363e4b] mb-8"
             type="email"
             name="email"
             id="email"
           />
-          <label htmlFor="password">password</label>
+          <label
+            className="text-[#95989b] text-sm font-semibold mb-3"
+            htmlFor="password"
+          >
+            password
+          </label>
           <input
+            className="outline-none rounded text-[#95989b] border-gray-600 border p-2 text-sm bg-[#363e4b]"
             placeholder="password.."
             type="password"
             name="password"
             id="password"
           />
-          <input type="checkbox" name="remember" id="remember" />
-          <label htmlFor="remember" className="text-sm">
-            Remember me
-          </label>
-          <button type="submit" className="">
+          <div className="flex gap-3 my-3 ">
+            <input type="checkbox" name="remember" id="remember" />
+            <label htmlFor="remember" className="text-sm text-gray-400">
+              Remember me
+            </label>
+          </div>
+          <button
+            type="submit"
+            className="bg-[#71b6f9]  mt-6 mb-16 rounded text-white p-2"
+          >
             Log in
           </button>
         </form>
       </div>
-    </>
+    </div>
   );
 }
-
-export default SignIn;
