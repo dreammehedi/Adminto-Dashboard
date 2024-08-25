13dc55769a8024180f89964d9614c153ed542418
mohotasir <mohotasir1036@gmail.com>
2024-08-25 22:25:29 +0600

    add: complete invoice page

--- src/mega_menu/pages/utility/Invoice.jsx
+++ src/mega_menu/pages/utility/Invoice.jsx
@@ -3,15 +3,15 @@ function Invoice() {
     <>
       {/* invoice page */}
       <section className="bg-secondary container text-gray-200 py-6">
-        <div className="bg-primary rounded shadow-lg  mx-auto p-8">
-          <div className="flex justify-between text-2xl font-semibold border-b border-gray-700 pb-6">
+        <div className="bg-primary rounded shadow-lg  mx-auto p-8 *:flex *:flex-col *:gap-4 md:*:flex-row *:justify-between">
+          <div className="text-2xl font-semibold border-b border-gray-700 pb-6">
             <h1 className="">Adminto</h1>
             <h1 className="text-base">
               Invoice # <br></br>
               2016-04-23654789
             </h1>
           </div>
-          <div className="flex justify-between text-sm text-gray-400 my-12">
+          <div className=" text-sm text-gray-400 my-12">
             <div>
               <p className="font-semibold">Twitter, Inc.</p>
               <p>795 Folsom Ave, Suite 600</p>
@@ -42,49 +42,86 @@ function Invoice() {
                   <th>Total</th>
                 </tr>
               </thead>
-              <tbody className= "text-center font-light *:border-b *:border-gray-700">
+              <tbody className="text-center font-light *:border-b *:border-gray-700">
                 <tr className="*:py-3 *:px-6">
-                  <td >1</td>
-                  <td >LCD</td>
-                  <td >Lorem ipsum dolor sit amet.</td>
-                  <td >1</td>
-                  <td >$380</td>
-                  <td >$380</td>
+                  <td>1</td>
+                  <td>LCD</td>
+                  <td>Lorem ipsum dolor sit amet.</td>
+                  <td>1</td>
+                  <td>$380</td>
+                  <td>$380</td>
                 </tr>
                 <tr className="*:py-3 *:px-6">
-                  <td >2</td>
-                  <td >Mobile</td>
-                  <td >Lorem ipsum dolor sit amet.</td>
-                  <td >5</td>
-                  <td >$50</td>
-                  <td >$250</td>
+                  <td>2</td>
+                  <td>Mobile</td>
+                  <td>Lorem ipsum dolor sit amet.</td>
+                  <td>5</td>
+                  <td>$50</td>
+                  <td>$250</td>
                 </tr>
                 <tr className="*:py-3 *:px-6">
-                  <td >3</td>
-                  <td >LED</td>
-                  <td >Lorem ipsum dolor sit amet.</td>
-                  <td >2</td>
-                  <td >$500</td>
-                  <td >$1000</td>
+                  <td>3</td>
+                  <td>LED</td>
+                  <td>Lorem ipsum dolor sit amet.</td>
+                  <td>2</td>
+                  <td>$500</td>
+                  <td>$1000</td>
                 </tr>
                 <tr className="*:py-3 *:px-6">
-                  <td >4</td>
-                  <td >LCD</td>
-                  <td >Lorem ipsum dolor sit amet.</td>
-                  <td >3</td>
-                  <td >$300</td>
-                  <td >$900</td>
+                  <td>4</td>
+                  <td>LCD</td>
+                  <td>Lorem ipsum dolor sit amet.</td>
+                  <td>3</td>
+                  <td>$300</td>
+                  <td>$900</td>
                 </tr>
                 <tr className="*:py-3 *:px-6">
-                  <td >5</td>
-                  <td >Mobile</td>
-                  <td >Lorem ipsum dolor sit amet.</td>
-                  <td >5</td>
-                  <td >$80</td>
-                  <td >$400</td>
+                  <td>5</td>
+                  <td>Mobile</td>
+                  <td>Lorem ipsum dolor sit amet.</td>
+                  <td>5</td>
+                  <td>$80</td>
+                  <td>$400</td>
                 </tr>
               </tbody>
             </table>
+          </div>
+          <div className="border-b border-gray-700 pb-8">
+            <div className="md:w-1/2 my-12 text-xs text-gray-400">
+              <h1 className="font-semibold mb-2">PAYMENT TERMS AND POLICIES</h1>
+              <p className="font-light">
+                All accounts are to be paid within 7 days from receipt of
+                invoice. To be paid by cheque or credit card or direct payment
+                online. If account is not paid within 7 days the credits details
+                supplied as confirmation of work undertaken will be charged the
+                agreed quoted fee noted above.
+              </p>
+            </div>
+            <div className="text-sm ">
+              <div className="flex flex-col gap-3 my-4 text-gray-400 border-b border-gray-600 pb-6">
+                <h1 className="font-semibold text-[15px]">
+                  Sub-total : <span>2390</span>
+                </h1>
+                <h1>
+                  Discount : <span>12.9%</span>
+                </h1>
+                <h1>
+                  VAT : <span>12.9%</span>
+                </h1>
+              </div>
+              <div className="font-semibold text-2xl">USD 1290</div>
+            </div>
+          </div>
+          <div className="mt-6">
+            <div></div>
+            <div className="">
+              <button className="p-2 text-sm mr-2 text-black rounded bg-white">
+                print
+              </button>
+              <button className="bg-blue-400 text-white px-3 py-2 text-sm font-semibold rounded">
+                Submit
+              </button>
+            </div>
           </div>
         </div>
       </section>
