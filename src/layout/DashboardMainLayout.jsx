a464b756e1d31e4ac807ad5ed4edb88a28dcf41c
dreammehedi <dreammehedihassan@gmail.com>
2024-08-26 19:12:40 +0600

    Pages Pagination Dashboard Pagination Implement

--- src/layout/DashboardMainLayout.jsx
+++ src/layout/DashboardMainLayout.jsx
@@ -1,19 +1,16 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import PagesPagination from "../shared/pages_pagination/PagesPagination";
 import Footer from "./../shared/footer/Footer";
 import Header from "./../shared/header/Header";
 
 function DashboardMainLayout() {
-  // get pathname
-  const { pathname } = useLocation();
-
   return (
     <>
       {/* header */}
       <Header></Header>
 
       {/* extra pages pagination */}
-      {pathname !== "/" && <PagesPagination></PagesPagination>}
+      <PagesPagination></PagesPagination>
 
       {/* outlet */}
       <Outlet></Outlet>
