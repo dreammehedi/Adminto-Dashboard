a8571c5fcc56d6163de3bcbcfaf9860e480696bf
DeveloperImran1 <abdulquddus01925251187@gmail.com>
2024-08-27 22:16:35 +0600

    chat page completed

--- src/mega_menu/apps/Chat.jsx
+++ src/mega_menu/apps/Chat.jsx
@@ -1,8 +1,22 @@
+import ChatBoldy from "../../components/chat/ChatBoldy";
+import ChatList from "../../components/chat/ChatList";
+
 function Chat() {
   return (
     <>
       {/* chat apps */}
-      <section>Chat</section>
+      <section className="flex flex-col lg:grid grid-cols-12 bg-secondary container text-white gap-8 pb-8">
+
+        {/* left side  */}
+        <div className="col-span-3  bg-primary rounded">
+          <ChatList></ChatList>
+        </div>
+
+        {/* right site  */}
+        <div className="col-span-9  bg-primary rounded ">
+          <ChatBoldy></ChatBoldy>
+        </div>
+      </section>
     </>
   );
 }
