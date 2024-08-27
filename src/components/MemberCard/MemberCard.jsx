ad07b5839c80d899fef0b15e180f742ea01cb1ff
dreammehedi <dreammehedihassan@gmail.com>
2024-08-27 17:14:44 +0600

    MemberList Page Update

--- src/components/MemberCard/MemberCard.jsx
+++ src/components/MemberCard/MemberCard.jsx
@@ -2,36 +2,38 @@ import PropTypes from "prop-types";
 export default function MemberCard({ member }) {
   const { bio, mobile, location, fullName, email, profileImage } = member;
   return (
-    <div className="text-sm bg-primary p-6 rounded-md text-gray-400 h-[410px]">
-      <div className="flex justify-center items-center flex-col">
-        <img
-          className="w-24 h-24 rounded-full border-2 border-gray-600 "
-          src={profileImage}
-          alt=""
-        />
-        <h1 className="text-center my-6">{bio}</h1>
+    <>
+      <div className="text-sm bg-primary p-4 md:p-6 rounded-md text-text-color h-fit">
+        <div className="flex justify-center items-center flex-col">
+          <img
+            className="size-24 rounded-full border-2 border-gray-600 object-cover my-transition hover:scale-[1.04] hover:cursor-pointer"
+            src={profileImage}
+            alt=""
+          />
+          <p className="text-center my-6 text-text-color text-xs">{bio}</p>
+        </div>
+        <div className="flex flex-col gap-3 text-sm">
+          <div>
+            <span className="font-semibold">Full Name : </span>
+            {fullName}
+          </div>
+          <div>
+            <span className="font-semibold">Email: </span> {email}
+          </div>
+          <div>
+            <span className="font-semibold">Mobile: </span> {mobile}
+          </div>
+          <div>
+            <span className="font-semibold">Location: </span> {location}
+          </div>
+        </div>
+        <div className="text-center">
+          <button className="px-4 py-2 bg-blue-400 rounded-full text-white my-3 hover:bg-blue-400/50 my-transition">
+            Send Message
+          </button>
+        </div>
       </div>
-      <div className="flex flex-col gap-4">
-        <h1>
-          <span className="font-semibold">Full Name : </span>
-          {fullName}
-        </h1>
-        <h1>
-          <span className="font-semibold">Email: </span> {email}
-        </h1>
-        <h1>
-          <span className="font-semibold">Mobile: </span> {mobile}
-        </h1>
-        <h1>
-          <span className="font-semibold">Location: </span> {location}
-        </h1>
-      </div>
-      <div className="text-center">
-        <button className="px-4 py-2 bg-blue-400 rounded-full text-white my-3 hover:bg-blue-500 my-transition">
-          Send Message
-        </button>
-      </div>
-    </div>
+    </>
   );
 }
 MemberCard.propTypes = {
--- src/mega_menu/apps/contacts/MembersList.jsx
+++ src/mega_menu/apps/contacts/MembersList.jsx
@@ -1,8 +1,7 @@
 import { IoMdAddCircleOutline } from "react-icons/io";
 const memberListData = [
   {
-    profileImage:
-      "https://st3.depositphotos.com/3431221/13621/v/450/depositphotos_136216036-stock-illustration-man-avatar-icon-hipster-character.jpg",
+    profileImage: "https://i.ibb.co/61K8wXk/ahidur-rahman.jpg",
     fullName: "Johnathan Deo",
     mobile: "(123) 123 1234",
     email: "coderthemes@gmail.com",
@@ -10,17 +9,15 @@ const memberListData = [
     bio: "Hi, I am Johnathan Deo, the industry's standard dummy text since the 1500s, when an unknown printer took a galley of type.",
   },
   {
-    profileImage:
-      "https://st3.depositphotos.com/3431221/13621/v/450/depositphotos_136216036-stock-illustration-man-avatar-icon-hipster-character.jpg",
-    fullName: "Johnathan Deo",
-    mobile: "(123) 123 1234",
-    email: "coderthemes@gmail.com",
-    location: "USA",
-    bio: "Hi, I am Johnathan Deo, bringing you dummy text from the 1500s.",
+    profileImage: "https://i.ibb.co/WsXJWvN/favicon.png",
+    fullName: "Md. Mehedi Hassan",
+    mobile: "+8801830143234",
+    email: "dreammehedihassan@gmail.com",
+    location: "Mirzapur, Tangail",
+    bio: "Hi, I am Md. Mehedi Hassan. I am MERN Stack Developer.",
   },
   {
-    profileImage:
-      "https://st3.depositphotos.com/3431221/13621/v/450/depositphotos_136216036-stock-illustration-man-avatar-icon-hipster-character.jpg",
+    profileImage: "https://i.ibb.co/61K8wXk/ahidur-rahman.jpg",
     fullName: "Johnathan Deo",
     mobile: "(123) 123 1234",
     email: "coderthemes@gmail.com",
@@ -28,13 +25,12 @@ const memberListData = [
     bio: "Hi, I am Johnathan Deo, standard dummy text specialist since the 1500s.",
   },
   {
-    profileImage:
-      "https://st3.depositphotos.com/3431221/13621/v/450/depositphotos_136216036-stock-illustration-man-avatar-icon-hipster-character.jpg",
-    fullName: "Johnathan Deo",
-    mobile: "(123) 123 1234",
-    email: "coderthemes@gmail.com",
-    location: "USA",
-    bio: "Hi, I am Johnathan Deo, your go-to for industry-standard dummy text.",
+    profileImage: "https://i.ibb.co/WsXJWvN/favicon.png",
+    fullName: "Md. Mehedi Hassan",
+    mobile: "+8801830143234",
+    email: "dreammehedihassan@gmail.com",
+    location: "Mirzapur, Tangail",
+    bio: "Hi, I am Md. Mehedi Hassan. I am MERN Stack Developer.",
   },
   {
     profileImage:
@@ -64,49 +60,12 @@ const memberListData = [
     bio: "Hi, I am Johnathan Deo, offering standard dummy text since the 1500s.",
   },
   {
-    profileImage:
-      "https://st3.depositphotos.com/3431221/13621/v/450/depositphotos_136216036-stock-illustration-man-avatar-icon-hipster-character.jpg",
-    fullName: "Johnathan Deo",
-    mobile: "(123) 123 1234",
-    email: "coderthemes@gmail.com",
-    location: "USA",
-    bio: "Hi, I am Johnathan Deo, maintaining the industry standard for dummy text.",
-  },
-  {
-    profileImage:
-      "https://st3.depositphotos.com/3431221/13621/v/450/depositphotos_136216036-stock-illustration-man-avatar-icon-hipster-character.jpg",
-    fullName: "Johnathan Deo",
-    mobile: "(123) 123 1234",
-    email: "coderthemes@gmail.com",
-    location: "USA",
-    bio: "Hi, I am Johnathan Deo, maintaining the industry standard for dummy text.",
-  },
-  {
-    profileImage:
-      "https://st3.depositphotos.com/3431221/13621/v/450/depositphotos_136216036-stock-illustration-man-avatar-icon-hipster-character.jpg",
-    fullName: "Johnathan Deo",
-    mobile: "(123) 123 1234",
-    email: "coderthemes@gmail.com",
-    location: "USA",
-    bio: "Hi, I am Johnathan Deo, maintaining the industry standard for dummy text.",
-  },
-  {
-    profileImage:
-      "https://st3.depositphotos.com/3431221/13621/v/450/depositphotos_136216036-stock-illustration-man-avatar-icon-hipster-character.jpg",
-    fullName: "Johnathan Deo",
-    mobile: "(123) 123 1234",
-    email: "coderthemes@gmail.com",
-    location: "USA",
-    bio: "Hi, I am Johnathan Deo, maintaining the industry standard for dummy text.",
-  },
-  {
-    profileImage:
-      "https://st3.depositphotos.com/3431221/13621/v/450/depositphotos_136216036-stock-illustration-man-avatar-icon-hipster-character.jpg",
-    fullName: "Johnathan Deo",
-    mobile: "(123) 123 1234",
-    email: "coderthemes@gmail.com",
-    location: "USA",
-    bio: "Hi, I am Johnathan Deo, the industry's standard dummy text creator.",
+    profileImage: "https://i.ibb.co/WsXJWvN/favicon.png",
+    fullName: "Md. Mehedi Hassan",
+    mobile: "+8801830143234",
+    email: "dreammehedihassan@gmail.com",
+    location: "Mirzapur, Tangail",
+    bio: "Hi, I am Md. Mehedi Hassan. I am MERN Stack Developer.",
   },
 ];
 
@@ -119,10 +78,10 @@ function MembersList() {
         <div className="container">
           {/*  navbar with sort and search */}
           <div className="p-6 rounded bg-primary flex flex-col gap-4 md:flex-row justify-between items-center mb-4">
-            <button className="flex items-center gap-1 text-white bg-green-600 py-2 px-3 rounded text-sm">
+            <button className="flex items-center gap-1 text-white bg-green-600 py-2 px-3 rounded text-sm my-transition hover:bg-green-500/50">
               <IoMdAddCircleOutline /> Add contact
             </button>
-            <div className="text-gray-400 text-sm flex gap-3 items-center">
+            <form className="text-gray-400 text-sm flex gap-3 items-center">
               <span>Sort by</span>
               <select
                 className="bg-none outline-none rounded py-2 px-3 text-gray-400  bg-primary border border-gray-600 focus:border-gray-500"
@@ -131,20 +90,21 @@ function MembersList() {
               >
                 <option value="All">All</option>
                 <option value="Name">Name</option>
-                <option value="Followers">Followers</option>
+                <option value="Followers">Post</option>
                 <option value="Following">Following</option>
               </select>
               <input
                 className="bg-none outline-none rounded p-2 text-gray-400  bg-primary border border-gray-600 focus:border-gray-500"
-                placeholder="search.."
+                placeholder="Search.."
                 type="text"
                 name="search"
-                id=""
+                id="search"
               />
-            </div>
+            </form>
           </div>
+
           {/* user card list section */}
-          <div className="grid grid-cols-1 gap-3 pb-6 md:grid-cols-2 lg:grid-cols-4 justify-center items-center">
+          <div className="grid grid-cols-1 gap-4 md:gap-6 lg:gap-8 md:grid-cols-2 lg:grid-cols-3 pb-6">
             {memberListData.map((member, ind) => (
               <MemberCard key={ind} member={member}></MemberCard>
             ))}
