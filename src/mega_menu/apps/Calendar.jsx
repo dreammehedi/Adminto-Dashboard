c58f18f8ff5f566b65331911c7647ee64f7a3303
dreammehedi <dreammehedihassan@gmail.com>
2024-08-27 17:57:32 +0600

    Simple Calender Apps Added

--- src/mega_menu/apps/Calendar.jsx
+++ src/mega_menu/apps/Calendar.jsx
@@ -1,8 +1,17 @@
+import moment from "moment";
+import { Calendar as BigCalendar, momentLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
 function Calendar() {
+  const localizer = momentLocalizer(moment);
   return (
     <>
       {/* calendar apps */}
-      <section>Calendar</section>
+      <section className="bg-secondary pb-6 !text-text-color">
+        <div className="container">
+          <BigCalendar localizer={localizer} style={{ height: 550 }} />
+        </div>
+      </section>
     </>
   );
 }
