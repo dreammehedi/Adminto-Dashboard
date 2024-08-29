7e9ddde14c829bf1ebaafbbc008603f7d945544d
dreammehedi <dreammehedihassan@gmail.com>
2024-08-29 16:13:08 +0600

    Calender Apps Custom Style Add

--- src/mega_menu/apps/Calendar.jsx
+++ src/mega_menu/apps/Calendar.jsx
@@ -1,31 +1,57 @@
 import moment from "moment";
+import { useState } from "react";
 import { Calendar as BigCalendar, momentLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-const events = [
-  {
-    start: moment("2023-03-18T10:00:00").toDate(),
-    end: moment("2023-03-18T11:00:00").toDate(),
-    title: "MRI Registration",
-  },
-  {
-    start: moment("2023-03-18T14:00:00").toDate(),
-    end: moment("2023-03-18T15:30:00").toDate(),
-    title: "ENT Appointment",
-  },
-];
-
+import "./calender.css";
 function Calendar() {
   const localizer = momentLocalizer(moment);
+  const [events, setEvents] = useState([
+    {
+      id: 0,
+      title: "Board meeting",
+      start: new Date(2024, 7, 29, 9, 0, 0),
+      end: new Date(2024, 7, 29, 13, 0, 0),
+      allDay: false,
+    },
+    {
+      id: 1,
+      title: "Lunch",
+      start: new Date(2024, 7, 30, 12, 0, 0),
+      end: new Date(2024, 7, 30, 13, 0, 0),
+      allDay: false,
+    },
+    {
+      id: 2,
+      title: "Conference",
+      start: new Date(2024, 7, 31, 9, 0, 0),
+      end: new Date(2024, 7, 31, 17, 0, 0),
+      allDay: true,
+    },
+  ]);
+  const handleSelectEvent = (event) => {
+    alert(`Event: ${event.title}`);
+  };
 
+  const handleSelectSlot = ({ start, end }) => {
+    const title = window.prompt("New Event name");
+    if (title) {
+      setEvents([...events, { start, end, title }]);
+    }
+  };
   return (
     <>
       {/* calendar apps */}
       <section className="bg-secondary pb-6 !text-text-color">
         <div className="container text-white">
           <BigCalendar
-            events={events}
             localizer={localizer}
-            style={{ height: 550 }}
+            events={events}
+            startAccessor="start"
+            endAccessor="end"
+            style={{ height: 500 }}
+            selectable
+            onSelectEvent={handleSelectEvent}
+            onSelectSlot={handleSelectSlot}
           />
         </div>
       </section>
